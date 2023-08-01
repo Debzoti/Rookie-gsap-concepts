808d8ae39d26550d2df589755778913b81a4d1bf
Debzoti <134799724+Debzoti@users.noreply.github.com>
2023-08-01 14:14:23 +0530

    Add files via upload
    It will upgrade day by day.

---
+++ script.js
@@ -0,0 +1,58 @@
+
+/*gsap.to("#box",{
+   x:500,
+   y:200,
+   duration:2,
+   delay:1,
+   backgroundColor:"purple",
+   rotate:360,
+   scale:0.5,
+});
+gsap.from("#box",{
+   x: 500,
+      y: 200,
+      duration: 2,
+      delay: 1,
+      backgroundColor: "purple",
+      rotate: 360,
+      scale: 0.5,
+   
+});
+gsap.to("h1",{
+   x:300,
+   duration:2,
+   delay:1,
+   color:"salmon",
+});
+gsap.to("h2", {
+   x: 300,
+   duration: 2,
+   delay: 2,
+   color: "cyan",
+});
+gsap.to("h3", {
+   x: 300,
+   duration: 2,
+   delay: 4,
+   color: "purple",
+});
+gsap.to("h1",{
+   x:300,
+   delay:1,
+   duration:2,
+  
+});
+gsap.to("h2", {
+   x: 300,
+   delay: 2,
+   duration: 2,
+   
+});
+gsap.to("h3", {
+   x: 300,
+   delay: 3,
+   duration: 2,
+   
+});*/
+
+
